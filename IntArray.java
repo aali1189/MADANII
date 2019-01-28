e681512620fe3a70f502895955190cf96c39c016
aali1189 <33586588+aali1189@users.noreply.github.com>
2019-01-28 12:54:47 +0800

    intarray
    int array

---
+++ IntArray.java
@@ -0,0 +1,34 @@
+
+package actitvity.cc3;
+
+import java.util.Scanner;
+
+public class IntArray {
+    
+    public static void main(String[] args){
+    
+    Scanner sc = new Scanner(System.in);
+    
+    int num[] = new int[5];
+    
+    
+    for(int i=0; i<num.length; i++){
+    System.out.print(" Enter an integer : ");
+    num[i] = sc.nextInt();
+    
+    }
+    
+    
+    System.out.print( "First to Last : ");
+    for(int i =0; i<num.length; i++){
+    System.out.print(num[i] + " ");
+    }
+ 
+    System.out.print( "\nLast to First : ");
+     for(int i =num.length-1; i>=0; i--){
+    System.out.print(num[i] + " ");
+    }
+    System.out.print( " \n");
+    }
+    
+}
